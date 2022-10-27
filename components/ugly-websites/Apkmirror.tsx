46658ee22165a4cc2fecbc0b9c7b7dce5fef1368
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-10-27 20:53:36 +0100

    add featured app banner

--- components/ugly-websites/Apkmirror.tsx
+++ components/ugly-websites/Apkmirror.tsx
@@ -1,9 +1,28 @@
 import { HiSearch, HiUpload, HiUserCircle } from "react-icons/hi"
+import { IoIosClose } from "react-icons/io"
+
+import Image from "next/image"
 
 const Apkmirror = () => {
   return (
     <div className="min-h-screen min-w-screen flex items-center">
-      <div className="bg-[#f5f5f5] h-[85vh] w-[400px] relative rounded-lg shadow-2xl overflow-hidden">
+      <div className="bg-[#f5f5f5] h-[85vh] w-[400px] relative rounded-xl shadow-2xl overflow-hidden border">
+        <div className="bg-[#f76912] w-[90%] h-max absolute left-1/2 top-8 -translate-x-1/2 rounded-lg text-[#f5f5f5] font-semibold flex items-center gap-4 overflow-hidden p-2 shadow-xl">
+          <Image
+            src={"/images/apkmirror-logo.png"}
+            width={50}
+            height={50}
+            alt="apkmirror logo"
+            className=""
+          />
+          <h1 className="text-sm">
+            Download APKMirror Installer (Official) to install .apkm bundles
+          </h1>
+          <span className="w-8 h-full shrink-0 cursor-pointer flex justify-center items-center ">
+            <IoIosClose className="text-4xl" />
+          </span>
+        </div>
+
         <div className="bg-[#f76912] absolute bottom-0 left-0 w-full h-16 flex  items-center justify-evenly text-4xl text-white">
           <HiSearch className=" cursor-pointer active:scale-90 transition-all" />
           <span className="bg-[#f5f5f5] text-[#f76912] w-[5rem] h-[5rem] rounded-full mb-14 border cursor-pointer">
