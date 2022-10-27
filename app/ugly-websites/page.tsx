ca689e8c72c27031cd703c73ffe0b1450f65df9e
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-10-27 15:07:57 +0100

    add ugly websites page

---
+++ app/ugly-websites/page.tsx
@@ -0,0 +1,13 @@
+export default () => {
+  return (
+    <div className="w-screen min-h-screen flex flex-col items-center justify-center">
+      <section className="w-screen h-screen flex flex-col items-center justify-center">
+        <h1 className="font-bold text-3xl p-4 uppercase">Ugly Websites</h1>
+        <p className="font-semibold text-xl">
+          In this section, I try to replicate old websites/apps designs then try
+          to improve them
+        </p>
+      </section>
+    </div>
+  )
+}
